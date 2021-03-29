7bb87ca6301c14f4e7ce28f891c02188eaba70a7
orestispanago <orestispanago@gmail.com>
2021-03-29 20:11:16 +0300

    Added second MAX31865 board, removed fault prints from loop()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MAX31865.h>
-#include <SPI.h>
 
 // Use software SPI: CS, DI, DO, CLK
 // Adafruit_MAX31865 maxe = Adafruit_MAX31865(15, 18, 4, 5); // As marked on board
 
 // use hardware SPI, just pass in the CS pin
-Adafruit_MAX31865 maxe = Adafruit_MAX31865(15); // As marked on board
 // Use print_spi_pins() to find your board defaults
-
+Adafruit_MAX31865 temp1 = Adafruit_MAX31865(15); // As marked on board
+// Adafruit_MAX31865 temp2 = Adafruit_MAX31865(4);
 
 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
 #define RREF 430.0
@@ -33,58 +32,21 @@ void printSpiPins()
   Serial.println();
 }
 
+
 void setup()
 {
   Serial.begin(115200);
-  printSpiPins();
-  maxe.begin(MAX31865_3WIRE); // set to 2WIRE or 4WIRE as necessary
+  // printSpiPins();
+  temp1.begin(MAX31865_3WIRE); // set to 2WIRE or 4WIRE as necessary
+  // temp2.begin(MAX31865_3WIRE); // set to 2WIRE or 4WIRE as necessary
 }
 
 void loop()
 {
-  // uint16_t rtd = maxe.readRTD();
 
-  // Serial.print("RTD value: "); Serial.println(rtd);
-  // Serial.println(rtd);
-  // float ratio = rtd;
-  // ratio /= 32768;
-  // Serial.print("Ratio = "); Serial.println(ratio,8);
-  // Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = ");
-  Serial.println(maxe.temperature(RNOMINAL, RREF));
-
-  // Check and print any faults
-  uint8_t fault = maxe.readFault();
-  if (fault)
-  {
-    Serial.print("Fault 0x");
-    Serial.println(fault, HEX);
-    if (fault & MAX31865_FAULT_HIGHTHRESH)
-    {
-      Serial.println("RTD High Threshold");
-    }
-    if (fault & MAX31865_FAULT_LOWTHRESH)
-    {
-      Serial.println("RTD Low Threshold");
-    }
-    if (fault & MAX31865_FAULT_REFINLOW)
-    {
-      Serial.println("REFIN- > 0.85 x Bias");
-    }
-    if (fault & MAX31865_FAULT_REFINHIGH)
-    {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
-    }
-    if (fault & MAX31865_FAULT_RTDINLOW)
-    {
-      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
-    }
-    if (fault & MAX31865_FAULT_OVUV)
-    {
-      Serial.println("Under/Over voltage");
-    }
-    maxe.clearFault();
-  }
-
+  Serial.print("temp1 = ");
+  Serial.println(temp1.temperature(RNOMINAL, RREF));
+  // Serial.print("temp2 = ");
+  // Serial.println(temp2.temperature(RNOMINAL, RREF));
   delay(1000);
 }
